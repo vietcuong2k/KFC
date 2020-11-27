5e88d64814a1ed2544eb993a59fae46d9a7ca59c
Ngochinh-bapngo <ngo85870@gmail.com>
2020-11-27 16:11:23 +0700

    add folder KFC

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="Css/style.css">
+</head>
+
+<body>
+    <header>
+        <div class="wrapper">
+            <div class="container">
+                <div class="stripper">
+                    <div class="stripper__row">
+                        <div class="stripper__row--col"> </div>
+                        <div class="stripper__row--col"> </div>
+                        <div class="stripper__row--col"> </div>
+                        <div class="stripper__row--col"> </div>
+                        <div class="stripper__row--col"> </div>
+                        <div class="stripper__row--col"> </div>
+                        <div class="stripper__row--col"> </div>
+                        <div class="stripper__row--col"> </div>
+                        <div class="stripper__row--col"> </div>
+                    </div>
+                </div>
+                <div class="nav-bar">
+                    <div class="nav-bar__logo">
+                        <img src="Image/logo_pc.png" alt="">
+                    </div>
+                    <div class="nav-bar__login">
+                        <span>VN</span>
+                        <a href="#">| Đăng nhập</a>
+                    </div>
+                </div>
+
+            </div>
+            <div class="side-bar ">
+                <div class="container side-bar__menu">
+                    <div class="side-bar__left">
+                        <ul>
+                            <li><a href="#">Combo 1 người</a></li>
+                            <li><a href="#">Combo nhóm</a></li>
+                            <li><a href="#">món lẻ</a></li>
+                        </ul>
+                    </div>
+                    <div class="side-bar__right">
+                        <ul>
+                            <li class="side-bar__right--phone">
+                                <p>Hotline:</p> <span>19008668</span>
+                            </li>
+                            <li class="side-bar__right--cart">
+                                <a href=" #">0</a>
+                                <a href="#">: Giỏ hàng</a>
+                            </li>
+
+
+                        </ul>
+                    </div>
+                </div>
+
+            </div>
+            <div class="header__banner container ">
+                <img src="Image/banner4.png" alt="">
+            </div>
+            
+        </div>
+
+
+    </header>
+</body>
+
+</html>
